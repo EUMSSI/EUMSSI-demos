92ca0ab3c85251ac434c38391159489f79a82c6c
jbarcelovsn <jbarcelo@vsn.es>
2015-10-13 14:06:38 +0200

    Improved the main Search Solr filter query with some functionalities, Now support logical connectors AND, OR.

--- js/FilterManager.js
+++ js/FilterManager.js
@@ -196,12 +196,224 @@ _.extend(FilterManager.prototype, {
 		];
 
 		if(value){
+			var fqval = this._buildQuerySimpleSearchAssets(value);
+			console.log("### BUSQUEDA ###\n "+fqval);
 			_.each(searchFields,function(val){
-				searchQueries.push(val + ":" + _.escape(value));
+				searchQueries.push(val + ":" + fqval);
 			});
 		}
 
 		return searchQueries.join(" OR ");
+	},
+
+
+
+	_buildQuerySimpleSearchAssets: function(valorBusqueda) {
+		var fqval = "";
+
+		fqval = this._escapeSpecialChars(valorBusqueda);
+		fqval = this._buildTextSearchValue(fqval);
+
+		return fqval;
+	},
+
+	_escapeSpecialChars: function(valorBusqueda){
+		if (valorBusqueda != "*" ){
+			valorBusqueda = valorBusqueda.replace("\\", "\\\\");//Modificamos las '\' primero, porque para el resto de caracteres de escape se añaden '\'
+			valorBusqueda = valorBusqueda.replace("+", "\\+");
+			valorBusqueda = valorBusqueda.replace("-", "\\-");
+			valorBusqueda = valorBusqueda.replace("&", "\\&");
+			valorBusqueda = valorBusqueda.replace("|", "\\|");
+			valorBusqueda = valorBusqueda.replace("!", "\\!");
+			valorBusqueda = valorBusqueda.replace("{", "\\{");
+			valorBusqueda = valorBusqueda.replace("}", "\\}");
+			valorBusqueda = valorBusqueda.replace("[", "\\[");
+			valorBusqueda = valorBusqueda.replace("]", "\\]");
+			valorBusqueda = valorBusqueda.replace("^", "\\^");
+			valorBusqueda = valorBusqueda.replace(":", "\\:");
+			//valorBusqueda = valorBusqueda.Replace("\"", "\\\"");  //No escapamos para permitir búsquedas de frases
+			//valorBusqueda = valorBusqueda.Replace("~", @"\~");    //No escapamos, para permitir búsquedas de cercanía al user
+			//valorBusqueda = valorBusqueda.Replace("*", @"\*");    //No escapamos, para permitir búsquedas con wildcards al user
+			//valorBusqueda = valorBusqueda.Replace("?", @"\?");    //No escapamos, para permitir búsquedas con wildcards al user
+			//valorBusqueda = valorBusqueda.Replace("(", @"\(");	//No escapamos, para permitir búsquedas con agrupaciones Ej: a AND (b OR c)
+			//valorBusqueda = valorBusqueda.Replace(")", @"\)");	//No escapamos, para permitir búsquedas con agrupaciones Ej: a AND (b OR c)
+
+			valorBusqueda = this._checkUseNearOperator(valorBusqueda);
+			valorBusqueda = this._checkUseWildcards(valorBusqueda);
+			valorBusqueda = this._checkWrongUseLogicalOperators(valorBusqueda);
+		}
+		return valorBusqueda;
+
+	},
+
+	/**
+	 * Extracheck
+	 * Cuando se utiliza el operador de cercanía, los términos de búsqueda deben ir entre ", pq si no solr interpreta el operador ~
+	 * como si fuera un FuzzyQuery, y si el valor de cercanía es mayor que 1 (que seguramente lo sea, porque de lo contrario no tiene
+	 * mucho sentido), el query a Solr da ERROR de parseo y dice: Minimum similarity for a FuzzyQuery has to be between 0.0f and 1.0f !
+	 * Si el user utiliza el operador ~, comprobar que los términos los ha colocado entre ", si no, añadirlas!!
+	 * @param valorBusqueda
+	 * @returns {*}
+	 * @private
+	 */
+	_checkUseNearOperator: function(valorBusqueda){
+		var index = valorBusqueda.indexOf('~');
+		if (index >= 0) {
+			if (valorBusqueda[index - 1] != '\"'){
+				valorBusqueda = valorBusqueda.slice(0,index) + "\"" + valorBusqueda.slice(index + 1);
+			}
+			if (valorBusqueda[0] != '\"'){
+				valorBusqueda = "\"" + valorBusqueda;
+			}
+		}
+		return valorBusqueda;
+	},
+
+	/**
+	 * @param valorBusqueda
+	 * @returns {string}
+	 * @private
+	 */
+	_checkUseWildcards: function(valorBusqueda){
+		var newValorBusqueda = "";
+		// Extracheck
+		// COPIADO DE MAM
+		// Hay un bug en esta versión de solr, que cuando se usan los wildcards * o ?, la búsqueda es caseSensitive.
+		// Lo convertimos todo a lowercase para evitar este problema, hasta que actualicemos versión de solr.
+		// Ojo con convertir a lowercase operadores lógicos!!!!
+		if (valorBusqueda.indexOf('*') >= 0 || valorBusqueda.indexOf('?') >= 0) {
+			var arrayTokens = valorBusqueda.split(' ');
+			for(var token in arrayTokens) {
+				if (token === "AND" || token === "OR" || token === "NOT"){
+					newValorBusqueda += token;
+				} else {
+					newValorBusqueda += token.toLowerCase();
+				}
+				newValorBusqueda += " ";
+			}
+		} else {
+			newValorBusqueda = valorBusqueda;
+		}
+		return newValorBusqueda;
+	},
+
+	/**
+	 * Extracheck
+	 * @param valorBusqueda
+	 * @returns {*}
+	 * @private
+	 */
+	_checkWrongUseLogicalOperators: function(valorBusqueda) {
+		var strVal = valorBusqueda.trim();
+		if (strVal === "NOT") valorBusqueda = valorBusqueda.toLowerCase();
+		if (strVal === "AND") valorBusqueda = valorBusqueda.toLowerCase();
+		if (strVal === "OR") valorBusqueda = valorBusqueda.toLowerCase();
+		return valorBusqueda;
+	},
+
+	_buildTextSearchValue: function(valorBusqueda) {
+		var formattedValue = "";
+
+		if (this._esBusquedaExacta(valorBusqueda) || this._containsAnyPhraseQuery(valorBusqueda))
+			formattedValue = valorBusqueda;
+		else if (this._containsNearOperator(valorBusqueda) || this._containsWilcards(valorBusqueda))
+			formattedValue = valorBusqueda;
+		else if (this._containsLogicalOperators(valorBusqueda))
+			formattedValue = this._includePhraseBoostingInSearchValue(valorBusqueda);
+		else formattedValue = this._makeMandatoryAllTokensInSearchValue(valorBusqueda);
+
+		return formattedValue;
+	},
+
+	_esBusquedaExacta: function(valorBusqueda){
+		var ret = false;
+		var aux = valorBusqueda.trim();
+		var arrayTokens = valorBusqueda.split('"');
+
+		if (( aux[0] == '"') &&
+			( aux[aux.length - 1] == '"') &&
+			(arrayTokens.length == 1))
+			ret = true;
+
+		return ret;
+	},
+
+	_containsAnyPhraseQuery: function(valorBusqueda){
+		var ret = false;
+		if (valorBusqueda.indexOf('"') >= 0)
+			ret = true;
+		return ret;
+	},
+
+	_containsNearOperator: function(valorBusqueda){
+		var ret = false;
+		if (valorBusqueda.indexOf('~') >= 0)
+			ret = true;
+		return ret;
+	},
+
+	_containsWilcards: function(valorBusqueda){
+		var ret = false;
+		if (valorBusqueda.indexOf('*') >= 0 || valorBusqueda.indexOf('?') >= 0)
+			ret = true;
+		return ret;
+	},
+
+	_containsLogicalOperators: function(valorBusqueda){
+		var ret = false;
+		if (valorBusqueda.indexOf('AND') >= 0 || valorBusqueda.indexOf('OR') >= 0)
+			ret = true;
+		return ret;
+	},
+
+	_includePhraseBoostingInSearchValue: function(valorBusqueda){
+		var strResult = valorBusqueda;
+		var temp = "";
+
+		if (valorBusqueda.indexOf(" AND ") >= 0 && valorBusqueda.indexOf(" OR ") < 0 && valorBusqueda.indexOf(" NOT ") < 0)	//Sólo ANDs
+		{
+			temp = valorBusqueda.replace(" AND ", " ");
+			if (temp)
+				strResult = '("{0}"~1000000)'.replace("{0}",temp);
+		}
+		else if (valorBusqueda.indexOf(" OR ") >= 0 && valorBusqueda.indexOf(" AND ") < 0 && valorBusqueda.indexOf(" NOT ") < 0) //Sólo ORs
+		{
+			temp = valorBusqueda.replace(" OR ", " ");
+			if (temp)
+				strResult = '{0} OR "{1}"~1000000^10'.replace("{0}",valorBusqueda).replace("{1}",temp);
+		}
+		return strResult;
+	},
+
+	/**
+	 * Buscamos cada uno de los strings que componen el valoBusqueda, separados por espacios en blanco.
+	 * A cada uno de estoos tokens, le añadimos el operador "AND" delante, para convertirlo en obligatorio.
+	 * Así lo que hará solr al buscar es un AND de todos los tokens que componen el valor a buscar.
+	 * @param valorBusqueda
+	 * @returns {string}
+	 * @private
+	 */
+	_makeMandatoryAllTokensInSearchValue: function(valorBusqueda){
+		var strResult = "";
+		var arrayTokens = valorBusqueda.trim().split(" ");
+
+		for (var i = 0; i < arrayTokens.length ; i++)
+		{
+			var token = arrayTokens[i];
+			var trimToken = token.trim(); //Trimamos blancos a principio y final de token
+
+			if (trimToken)
+			{
+				if (i == 0) strResult +=  trimToken;
+				else strResult += " AND {0}".replace("{0}",trimToken);
+			}
+		}
+
+		//Este sería el caso en el que el usuario está introduciendo espacios en blanco al principo de la cadena de búsqueda
+		if (!strResult) strResult = "*";
+		else strResult = this._includePhraseBoostingInSearchValue(strResult);	//Boosting de las frases, para q tokens más cercanos tengan más score
+
+		return strResult;
 	}
 
 });
