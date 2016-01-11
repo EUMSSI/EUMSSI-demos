c328ed1aa7c3d2cfd4fc7a30503d3792d60dee7f
Giang Binh Tran <giangtranbinh@gmail.com>
2016-01-11 18:10:50 +0100

    x

--- js/widgets/GenericGraphWidget.js
+++ js/widgets/GenericGraphWidget.js
@@ -247,7 +247,7 @@
 				.links(links)
 				.size([width, height])
 				.linkDistance(300)
-				.charge(-50)
+				.charge(50)
 				.on("tick", tick)
 				.start();
 			// Append SVG to the html, with defined constants
